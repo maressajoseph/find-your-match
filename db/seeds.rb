827bc52b284cef48403458031e4b25980857778e
maressajoseph <maressa.joseph@gmail.com>
2017-06-06 13:53:43 +0200

    combinations in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@ student_b = User.create!(email: "ho@ho.com", password: "123456", admin: false)
 today = MatchDay.create!(day: Date.today)
 
 match1 = Match.create!(match_day: today, student1: student_a, student2: student_b)
+
+combinations = AvailableCombination.create!(combis: [])
