0c9c7a2defb6b316f83af83052fafa06c91364b7
maressajoseph <maressa.joseph@gmail.com>
2017-06-07 11:17:58 +0200

    Admin page for admin users promote/demote users to admin/student

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get '/adminpage' => 'users#index'
+    get '/adminpage/:id' => 'users#edit'
+    post '/adminpage.:id' => 'users#change_admin'
   end
 
   root to: 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
   resources :matchdays
 end
