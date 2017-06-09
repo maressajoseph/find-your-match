cb3d5b812a1745dbdd14b2edd5c243a885160843
bram <bramkaashoek@gmail.com>
2017-06-09 16:21:19 +0200

    wrote tests

--- spec/models/match_day_spec.rb
+++ spec/models/match_day_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe MatchDay, type: :model do
 
     let!(:match_day1) { create :match_day, group: group1, day: Date.today }
 
-    it "creates a matchday which includes 2 matches" do
+    it "4 users should make 2 matches" do
       expect(match_day1.matches.length).to equal(2)
     end
 
@@ -27,19 +27,35 @@ RSpec.describe MatchDay, type: :model do
 
     let!(:match_day2) { create :match_day, group: group1, day: Date.today + 1}
 
-    it "expects the dummy user to be included so there are 3 matches" do
+    it "5 users should include dummy user to make 3 matches" do
       expect(match_day2.matches.length).to equal(3)
     end
-
-    it "expects the matches from the first matchday to be different than the matches from the second matchday" do
-      expect(match_day1.matches).not_to equal(match_day1.matches)
-    end
-
-    let!(:more_matchdays) { create_list :match_day, 4, group: group1  }
-
-    it "expects n=(assignable_users)-1 matchdays to all have different matches" do
-      
-    end
-
   end
+
+  describe "the algorithm" do
+    DatabaseCleaner.clean
+
+    let!(:user1) { create :user }
+    let!(:user2) { create :user }
+    let!(:user3) { create :user }
+    let!(:user4) { create :user }
+
+    let!(:group1) { create :group }
+
+    let!(:match_day1) { create :match_day, group: group1, day: Date.today }
+    let!(:match_day2) { create :match_day, group: group1, day: Date.today + 1}
+    let!(:match_day3) { create :match_day, group: group1, day: Date.today + 2}
+    let!(:match_day4) { create :match_day, group: group1, day: Date.today + 3}
+    let!(:match_day5) { create :match_day, group: group1, day: Date.today + 4}
+
+
+
+    it "expects the matches to be the same every fourth day for 4 students" do
+
+    expect(match_day1.matches.map{ |m| m.student1.first_name}).not_to eq(match_day2.matches.map{ |m| m.student1.first_name})
+    expect(match_day1.matches.map{ |m| m.student1.first_name}).not_to eq(match_day3.matches.map{ |m| m.student1.first_name})
+    expect(match_day1.matches.map{ |m| m.student1.first_name}).to eq(match_day4.matches.map{ |m| m.student1.first_name})
+  end
+  end
+
 end
